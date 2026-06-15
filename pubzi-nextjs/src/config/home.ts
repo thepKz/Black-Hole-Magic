fc1db119c70c780779d0cdfed728b8e5f20e74a6
thepkz <maitanthepmrthep@gmail.com>
2026-06-15 09:55:18 +0700

    Add: add fast fast

--- pubzi-nextjs/src/config/home.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Cấu hình trang chủ
- * Chỉ sử dụng 1 phiên bản duy nhất - home-7 (Black Hole brand)
- */
-
-export type HomeVersion = 'home-7';
-
-// Black Hole sử dụng home-7 làm trang chủ duy nhất
-export const HOME_VERSION: HomeVersion = 'home-7';
